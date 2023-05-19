ba5c2dbfa1e3251bf56b1c3a91bb2c551f5a2ac5
Lumi <lumi.kallioniemi@proton.me>
2023-05-20 01:26:38 +0300

    Initial commit part deux
    Bot is now brought to basic functionality - we can now start
    implementing commands et cetera.

--- src/commandHandler/commands/exposeState.ts
+++ src/commandHandler/commands/exposeState.ts
@@ -1,10 +1,18 @@
 import { ArgumentsWithContext } from '..'
-import { addWatchee } from '../../redux/reducers/watchees'
 
 export const command = 'expose-state'
 export const describe = "exposes the bot's internal state"
 export const builder = {}
-export const handler = ({ message, dispatch }: ArgumentsWithContext) => {
-  message.reply('Hello world!')
-  dispatch(addWatchee({ twitchUsername: message.content }))
+export const handler = async ({ message, store }: ArgumentsWithContext) => {
+  const { getState } = store
+  const { watcheesSlice } = getState()
+  let watchees = watcheesSlice.reduce((acc, item) => {
+    acc = `${acc} ${item},`
+    return acc
+  }, '')
+  watchees = watchees.slice(0, -1) + '.'
+  message.reply(
+    `Number of watchees: ${watcheesSlice.length}` +
+      (watcheesSlice.length > 0 ? `\nWatchees: ${watchees}` : '')
+  )
 }
